6d428697e102a7a920c35b47efe1aa3329379a45
swatisingh16 <84076296+swatisingh16@users.noreply.github.com>
2021-07-27 11:43:17 +0530

    product.cpp
    find product of two numbers entered by user

---
+++ product.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float n1, n2, product;
+
+    cout<<"Enter two numbers: "<< endl;
+    cin>>n1>>n2;
+    
+    product = n1*n2;
+
+    cout<<"Product = "<< product <<endl;
+    
+    return 0;
+}
