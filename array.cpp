09629b36b9ce2f4db40f63dacacf89527aef0548
swatisingh16 <84076296+swatisingh16@users.noreply.github.com>
2021-07-27 12:59:42 +0530

    array.cpp

---
+++ array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter Elements:"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"You Entered:"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+    return 0;
+}
